3c68690fdf7e835ba54ea3ced4c1a122ac0d0722
Johnny <johnnyrogers29@gmail.com>
2023-10-26 12:10:00 +0100

    c3: Add initial profile with DB

--- app/navigator/TabNavigator.tsx
+++ app/navigator/TabNavigator.tsx
@@ -1,4 +1,3 @@
-import { View, Text } from 'react-native'
 import React from 'react'
 import Home from '../screens/Home';
 import Profile from '../screens/Profile';
--- app/screens/Profile.tsx
+++ app/screens/Profile.tsx
@@ -1,11 +1,41 @@
-import { View, Text } from "react-native";
+import { setDoc } from "firebase/firestore";
+import { useState } from "react";
+import { View, Text, TextInput, Button } from "react-native";
+import { FIREBASE_DB } from "../../firebaseConfig";
 
 const Profile = () => {
+    const [name, setName] = useState("");
+
+    const Submit =async () => {
+        
+        try{
+            const response = await setDoc(doc(FIREBASE_DB, "profile", ))
+        }
+    }
+
     return(
         <View>
-            <Text>
-                Profile tab
-            </Text>
+            <Text>Profile tab</Text>
+            
+            <Text>Name</Text>
+            <TextInput
+                value={name}
+                placeholder="Name"
+                onChangeText={setName}
+            />
+            <Text>Address</Text>
+            <TextInput
+                placeholder="Address"
+            />
+            <Text>Occupation</Text>
+            <TextInput
+                placeholder="Occupation"
+            />
+            <Text>Tax Details</Text>
+            <TextInput
+                placeholder="Tax details"
+            />
+            <Button title='Submit' onPress={Submit}/> 
         </View>
     )
 }
